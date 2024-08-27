c3ceebc0d000754de4602d373b1e01faf601e04e
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-08-28 00:36:42 +0200

    FEAT: added error catching - Mikhail Dorokhovich

--- compositor/adapters/test_adapter/src/adapter.py
+++ compositor/adapters/test_adapter/src/adapter.py
@@ -39,57 +39,66 @@ class Adapter:
             })
 
     async def scrape(self, task_id, configuration):
-        async with async_playwright() as p:
-            self.publish_to_kafka(self.kafka_config.get("topik"), "test_adapter", {
+        try:
+            async with async_playwright() as p:
+                self.publish_to_kafka(self.kafka_config.get("topik"), "update_task", {
+                    "task_id": task_id,
+                    "status": "PROCESSING",
+                })
+
+                browser = await p.chromium.launch(headless=True)
+                page = await browser.new_page()
+                artifacts = []
+                await page.goto(configuration['url'])
+                await page.wait_for_selector(configuration['wait_for_selector'])
+
+                platform = find_or_create_node(Platform, {"name": 'test-platform'})
+                scrapper = find_or_create_node(Scrapper, {"name": 'TestAdapter'})
+                url_node = create_url_node(configuration['url'])
+                url_node.access_time = datetime.now()
+                url_node.found_at.connect(platform)
+                url_node.scrapped_by.connect(scrapper)
+
+                url_node.save()
+
+                artifacts.extend([platform, scrapper, url_node])
+
+                for selector in configuration['selectors']:
+                    product_nodes = await page.query_selector_all(selector['selector'])
+
+                    for node in product_nodes:
+                        if selector['extract_from'] == "attribute":
+                            value = await node.get_attribute(selector['attribute_name'])
+                        elif selector['extract_from'] == "text":
+                            value = await node.inner_text()
+
+                        neo_node = find_or_create_node(Artifact, {
+                            "artifact_type": selector['artifact_type'],
+                            "artifact_property": selector['artifact_attribute'],
+                            "artifact_value": value,
+                            "metadata": selector
+                        })
+
+                        neo_node.containing_url.connect(url_node)
+
+                        artifacts.extend([neo_node])
+
+                self.publish_to_kafka(self.kafka_config.get("topik"), "update_task", {
+                    "task_id": task_id,
+                    "status": "COMPLETED",
+                    "executed_at": datetime.now().isoformat(),
+                    "retries": 1,
+                    "artifacts": [artifact.element_id for artifact in artifacts]
+                })
+
+                await browser.close()
+        except Exception as e:
+            print(f"Failed to scrape {configuration['url']}: {e}")
+            self.publish_to_kafka(self.kafka_config.get("topik"), "update_task", {
                 "task_id": task_id,
-                "status": "PROCESSING",
-            })
-            browser = await p.chromium.launch(headless=True)
-            page = await browser.new_page()
-            artifacts = []
-            await page.goto(configuration['url'])
-            await page.wait_for_selector(configuration['wait_for_selector'])
-
-            platform = find_or_create_node(Platform, {"name": 'test-platform'})
-            scrapper = find_or_create_node(Scrapper, {"name": 'TestAdapter'})
-            url_node = create_url_node(configuration['url'])
-            url_node.access_time = datetime.now()
-            url_node.found_at.connect(platform)
-            url_node.scrapped_by.connect(scrapper)
-
-            url_node.save()
-
-            artifacts.extend([platform, scrapper, url_node])
-
-            for selector in configuration['selectors']:
-                product_nodes = await page.query_selector_all(selector['selector'])
-
-                for node in product_nodes:
-                    if selector['extract_from'] == "attribute":
-                        value = await node.get_attribute(selector['attribute_name'])
-                    elif selector['extract_from'] == "text":
-                        value = await node.inner_text()
-
-                    neo_node = find_or_create_node(Artifact, {
-                        "artifact_type": selector['artifact_type'],
-                        "artifact_property": selector['artifact_attribute'],
-                        "artifact_value": value,
-                        "metadata": selector
-                    })
-
-                    neo_node.containing_url.connect(url_node)
-
-                    artifacts.extend([neo_node])
-
-            self.publish_to_kafka(self.kafka_config.get("topik"), "test_adapter", {
-                "task_id": task_id,
-                "status": "COMPLETED",
+                "status": "FAILED",
                 "executed_at": datetime.now().isoformat(),
-                "retries": 1,
-                "artifacts": [artifact.element_id for artifact in artifacts]
             })
-
-            await browser.close()
 
     def publish_to_kafka(self, topic, key, value):
         try:
